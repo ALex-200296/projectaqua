a89ad1fb15b8e8f917d9460be0f75787c20a85b7
Александр Ли <li.aa2012@yandex.ru>
2021-02-17 15:46:00 +0300

    edit3

--- js/index.js
+++ js/index.js
@@ -8,3 +8,27 @@ $(document).ready(function () {
   $(this).children('ul').toggleClass('dropdown__service__list2-active')
  })
 });
+
+   // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [60.08259258995241,30.26536116633978],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 18,
+        })
+        myMap.geoObjects.add(  new ymaps.Placemark([60.08259258995241,30.26536116633978], {
+          iconLayout: 'default#image',
+          iconImageHref: '../image/placeholder.svg',
+          iconCaption: 'Выборское ш.,389, Санкт-Петербург ...',
+          iconImageSize: [30, 42],
+          iconImageOffset: [-3, -42],
+      },));
+    }
