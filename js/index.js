fdad6a68142e637b1b76537ca6178388f28ee68d
Александр Ли <li.aa2012@yandex.ru>
2021-03-03 14:42:27 +0300

    clean

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const item = document.querySelectorAll('.pages__questions__item');
-const asideItem = document.querySelectorAll('.aside__item')
+const asideItem = document.querySelectorAll('.aside__item');
 
 $(document).ready(function () {
   console.log('hello')
@@ -71,7 +71,9 @@ $(document).ready(function () {
   $('#nav__header__mobile').removeClass('header__top__nav__mobile-active')
 })
 
-window.addEventListener('scroll', ()=> {
+$(window).scroll(function (event) {
+  console.log($(window).scrollTop())
+ if($(window).scrollTop() >= 500) {
   $('#nav__header__mobile').removeClass('header__top__nav__mobile-active')
 
   if($('#svg1').hasClass('svg-active')) {
@@ -79,57 +81,74 @@ window.addEventListener('scroll', ()=> {
     $('#svg1').removeClass('svg-active')
     $('#svg2').addClass('svg-active')
   }
-})
+ }
+});
 });
 
-  //pages__questions
-  item.forEach( function(i) {
-    const li  = i.childNodes[1];
-    const p = i.childNodes[3];
-    li.addEventListener('click', () => {
-      if(p.style.maxHeight) {
-        p.style.maxHeight = null;
-      }else{
-        p.style.maxHeight = p.scrollHeight + 'px';
-      };
-    });
-  });
-  //pages__questions
-  //aside
-  asideItem.forEach(function(i) {
-    const btn = i.children[0]
-    const ul = i.children[1]
-    btn.addEventListener('click', ()=> {
-      if(ul.style.maxHeight) {
-        ul.style.maxHeight = null;
-      }else{
-        ul.style.maxHeight = ul.scrollHeight + 'px';
-      };
-    })
+//clean
+$('.clean4__center__item').each(function(i) {
+  const buttonOpen = this.children[0].children[0].children[0].children[1];
+  const buttonClose = this.children[1].children[0].children[1]
+  const div = this.children[1];
+
+  buttonOpen.addEventListener('click', function() {
+    div.style.display = 'block';
   })
+  buttonClose.addEventListener('click', function() {
+    div.style.display = 'none';
+  })
+})
 
-  //aside
+//clean--end  
 
-   // Функция ymaps.ready() будет вызвана, когда
-    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [60.08259258995241,30.26536116633978],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 18,
-        })
-        myMap.geoObjects.add(  new ymaps.Placemark([60.08259258995241,30.26536116633978], {
-          iconLayout: 'default#image',
-          iconImageHref: '../image/placeholder.svg',
-          iconCaption: 'Выборское ш.,389, Санкт-Петербург ...',
-          iconImageSize: [30, 42],
-          iconImageOffset: [-3, -42],
-      },));
-    }
+//pages__questions
+item.forEach( function(i) {
+  const li  = i.childNodes[1];
+  const p = i.childNodes[3];
+  li.addEventListener('click', () => {
+    if(p.style.maxHeight) {
+      p.style.maxHeight = null;
+    }else{
+      p.style.maxHeight = p.scrollHeight + 'px';
+    };
+  });
+});
+//pages__questions--end
+//aside
+asideItem.forEach(function(i) {
+  const btn = i.children[0]
+  const ul = i.children[1]
+  btn.addEventListener('click', ()=> {
+    if(ul.style.maxHeight) {
+      ul.style.maxHeight = null;
+    }else{
+      ul.style.maxHeight = ul.scrollHeight + 'px';
+    };
+  })
+})
+
+//aside--end
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [60.08259258995241,30.26536116633978],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 18,
+    })
+    myMap.geoObjects.add(  new ymaps.Placemark([60.08259258995241,30.26536116633978], {
+      iconLayout: 'default#image',
+      iconImageHref: '../image/placeholder.svg',
+      iconCaption: 'Выборское ш.,389, Санкт-Петербург ...',
+      iconImageSize: [30, 42],
+      iconImageOffset: [-3, -42],
+  },));
+}
